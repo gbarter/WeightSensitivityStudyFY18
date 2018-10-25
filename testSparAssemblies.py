d3f911ce23c947103cf00005a64a7abd7a081d18
gbarter <garrett.barter@nrel.gov>
2018-10-25 05:50:41 -0600

    keeping up with section chnges

---
+++ testSparAssemblies.py
@@ -0,0 +1,38 @@
+from wisdem.floating.turbine_spar_instance import TurbineSparInstance
+from wisdem.floating.turbine_semi_instance import TurbineSemiInstance
+from floatingse.instance.spar_instance import SparInstance
+from baselineSpar import mysetup
+import numpy as np
+
+subsave    = 'spar-soga.save'
+turbsave   = 'turb-spar-soga.save'
+
+
+mysub = SparInstance()
+mysub.set_reference('10MW')
+mysub.params['water_depth'] = 320.0
+mysub.params['Hs'] = 10.8
+mysub.params['T'] = 9.8
+mysub.params['Uref'] = 11.0
+mysub.params['zref'] = 119.0
+mysub.params['max_offset'] = 100.0
+mysub.evaluate()
+
+myturb = TurbineSparInstance('10MW')
+myturb.set_reference('10MW')
+myturb.params['water_depth'] = 320.0
+myturb.params['wave_height'] = 10.8
+myturb.params['wave_period'] = 9.8
+myturb.params['wind_reference_speed'] = 11.0
+myturb.params['wind_reference_height'] = 119.0
+myturb.evaluate()
+
+myturb = TurbineSemiInstance('10MW')
+myturb.set_reference('10MW')
+myturb.params['water_depth'] = 320.0
+myturb.params['wave_height'] = 10.8
+myturb.params['wave_period'] = 9.8
+myturb.params['wind_reference_speed'] = 11.0
+myturb.params['wind_reference_height'] = 119.0
+myturb.evaluate()
+
